9213326598f2dd1e93f2e38894762310f5d83e11
uptightsuperlabs <uptight283@gmail.com>
2026-02-24 11:36:06 -0500

    add nbt.hpp + tags

---
+++ src/nbt.hpp
@@ -0,0 +1,20 @@
+#ifndef HYSTRUCTURE_NBT
+#define HYSTRUCTURE_NBT
+
+// uptightsuperlabs - 2/23/2026 shout out to https://minecraft.wiki/w/NBT_format
+
+#define TAG_End 0x00
+#define TAG_Byte 0x01
+#define TAG_Short 0x02
+#define TAG_Int 0x03
+#define TAG_Long 0x04
+#define TAG_Float 0x05 
+#define TAG_Double 0x06
+#define TAG_Byte_Array 0x07
+#define TAG_String 0x08
+#define TAG_List 0x09
+#define TAG_Compound 0x0A
+#define TAG_Int_Array 0x0B
+#define TAG_Long_Array 0x0C
+
+#endif
